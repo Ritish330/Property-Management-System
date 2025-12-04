4d5d8d6d938157565156fa7517ddcc0bc5478572
Ritish330 <erc020@nist.edu>
2025-12-04 12:06:38 +0530

    Side bar and Top bar chnages

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,34 +1,34 @@
-// src/components/Sidebar.js
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import './Sidebar.css';
+  // src/components/Sidebar.js
+  import React from 'react';
+  import { NavLink } from 'react-router-dom';
+  import './Sidebar.css';
 
-const Sidebar = () => {
-  return (
-    <div className="sidebar">
-      <div className="sidebar-title">Su Admin Portal</div>
-      <nav className="sidebar-nav">
-        <NavLink to="/" end className="sidebar-link">
-          Dashboard
-        </NavLink>
-        <NavLink to="/property" className="sidebar-link">
-          Property
-        </NavLink>
-        <NavLink to="/room-types" className="sidebar-link">
-          Room Types
-        </NavLink>
-        <NavLink to="/rate-plans" className="sidebar-link">
-          Rate Plans
-        </NavLink>
-        <NavLink to="/availability" className="sidebar-link">
-          Availability
-        </NavLink>
-        <NavLink to="/reservations" className="sidebar-link">
-          Reservations
-        </NavLink>
-      </nav>
-    </div>
-  );
-};
+  const Sidebar = () => {
+    return (
+      <div className="sidebar">
+        <div className="sidebar-title">Su Admin Portal</div>
+        <nav className="sidebar-nav">
+          <NavLink to="/" end className="sidebar-link">
+            Dashboard
+          </NavLink>
+          <NavLink to="/property" className="sidebar-link">
+            Property
+          </NavLink>
+          <NavLink to="/room-types" className="sidebar-link">
+            Room Types
+          </NavLink>
+          <NavLink to="/rate-plans" className="sidebar-link">
+            Rate Plans
+          </NavLink>
+          <NavLink to="/availability" className="sidebar-link">
+            Availability
+          </NavLink>
+          <NavLink to="/reservations" className="sidebar-link">
+            Reservations
+          </NavLink>
+        </nav>
+      </div>
+    );
+  };
 
-export default Sidebar;
+  export default Sidebar;
